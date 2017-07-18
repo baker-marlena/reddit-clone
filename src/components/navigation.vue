032651b9abdc8beddda96a004ef4dfc63a60a8a8
Marlena Baker <baker.marlena@gmail.com>
2017-07-18 15:23:06 -0600

    initial commit

---
+++ src/components/navigation.vue
@@ -0,0 +1,21 @@
+<template>
+  <div id="navigation">
+    <nav class="navbar navbar-default">
+      <div class="container-fluid">
+        <div class="navbar-header">
+          <h3>Clone</h3>
+        </div>
+      </div>
+    </nav>
+  </div>
+</template>
+<script>
+export default {
+  name: "navigation",
+  data: () => ({
+
+  })
+}
+</script>
+<style scoped>
+</style>
